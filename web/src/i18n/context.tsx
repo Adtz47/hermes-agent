73a4f5e744a7b21f123d2e72af75a8048e49f122
Aditya Kumar Gour <aditya7980kumar@gmail.com>
2026-09-24 15:15:01 +0530

    style(web): format locale condition

--- web/src/i18n/context.tsx
+++ web/src/i18n/context.tsx
@@ -61,13 +61,13 @@ function getInitialLocale(): Locale {
     const browserLanguage = navigator.language.toLowerCase();
 
     if (
-  browserLanguage === "zh-tw" ||
-  browserLanguage === "zh-hk" ||
-  browserLanguage === "zh-mo" ||
-  browserLanguage.startsWith("zh-hant")
-) {
+      browserLanguage === "zh-tw" ||
+      browserLanguage === "zh-hk" ||
+      browserLanguage === "zh-mo" ||
+      browserLanguage.startsWith("zh-hant")
+    ) {
   return "zh-hant";
-}
+    }
 
     if (isLocale(browserLanguage)) return browserLanguage;
 
